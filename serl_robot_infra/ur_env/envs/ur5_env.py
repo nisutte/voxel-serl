4f9d205f1add15b8ee779c86bccd90f5d8939b3d
nisutte <nisutte@student.ethz.ch>
2025-06-27 15:29:23 +0200

    no truncation penalty in the single ur5 env

--- serl_robot_infra/ur_env/envs/ur5_env.py
+++ serl_robot_infra/ur_env/envs/ur5_env.py
@@ -356,7 +356,7 @@ class UR5Env(gym.Env):
 
         reward = self.compute_reward(obs, action)
         truncated = self._is_truncated()
-        reward = reward if not truncated else reward - 10.  # truncation penalty
+        # reward = reward if not truncated else reward - 10.  # truncation penalty
         done = self.curr_path_length >= self.max_episode_length or self.reached_goal_state(obs) or truncated
 
         dt = time.time() - start_time
