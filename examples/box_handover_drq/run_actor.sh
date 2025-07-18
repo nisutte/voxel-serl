fa87550a3963a72978526620fec4ed338a0de1b4
nisutte <nisutte@student.ethz.ch>
2025-07-18 15:40:49 +0200

    new run

--- examples/box_handover_drq/run_actor.sh
+++ examples/box_handover_drq/run_actor.sh
@@ -2,13 +2,13 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@" \
     --actor \
-    --exp_name="Handover V3" \
+    --exp_name="Handover V5" \
     --camera_mode pointcloud \
     --max_traj_length 100 \
-    --seed 0 \
+    --seed 42 \
     --max_steps 20000 \
     --random_steps 0 \
-    --training_starts 500 \
+    --training_starts 200 \
     --utd_ratio 8 \
     --batch_size 64 \
     --eval_period 0 \
--- examples/box_handover_drq/run_learner.sh
+++ examples/box_handover_drq/run_learner.sh
@@ -2,18 +2,18 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.5 && \
 python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@" \
     --learner \
-    --exp_name="Handover V3" \
+    --exp_name="Handover V5" \
     --camera_mode pointcloud \
     --max_traj_length 100 \
-    --seed 0 \
+    --seed 42 \
     --max_steps 25000 \
     --random_steps 0 \
-    --training_starts 500 \
+    --training_starts 200 \
     --utd_ratio 8 \
     --batch_size 64 \
     --checkpoint_period 1000 \
     --checkpoint_path /home/nico/real-world-rl/serl/examples/box_handover_drq/checkpoints \
-    --demo_path /home/nico/real-world-rl/serl/examples/box_handover_drq/box_picking_6_demos_jun27.pkl \
+    --demo_path /home/nico/real-world-rl/serl/examples/box_handover_drq/box_picking_5_demos_jul18.pkl \
     \
     --encoder_type voxnet-pretrained \
     --state_mask all \
