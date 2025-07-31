53bde938949792bf22e099924083c5960ac8faba
nisutte <nisutte@student.ethz.ch>
2025-07-31 18:10:14 +0200

    new successful run

--- examples/box_handover_drq/run_actor.sh
+++ examples/box_handover_drq/run_actor.sh
@@ -2,7 +2,7 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@" \
     --actor \
-    --exp_name="Handover V6" \
+    --exp_name="Handover V7 adapt voxel" \
     --camera_mode pointcloud \
     --max_traj_length 100 \
     --seed 42 \
@@ -15,5 +15,5 @@ python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@
     \
     --encoder_type voxnet-pretrained \
     --state_mask all \
-    --encoder_bottleneck_dim 128 \
+    --encoder_bottleneck_dim 64 \
 #    --debug
--- examples/box_handover_drq/run_learner.sh
+++ examples/box_handover_drq/run_learner.sh
@@ -2,7 +2,7 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.5 && \
 python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@" \
     --learner \
-    --exp_name="Handover V6" \
+    --exp_name="Handover V7 adapt voxel" \
     --camera_mode pointcloud \
     --max_traj_length 100 \
     --seed 42 \
@@ -13,9 +13,9 @@ python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@
     --batch_size 64 \
     --checkpoint_period 1000 \
     --checkpoint_path /home/nico/real-world-rl/serl/examples/box_handover_drq/checkpoints \
-    --demo_path /home/nico/real-world-rl/serl/examples/box_handover_drq/box_picking_10_demos_jul23.pkl \
+    --demo_path /home/nico/real-world-rl/serl/examples/box_handover_drq/box_picking_10_demos_jul31.pkl \
     \
     --encoder_type voxnet-pretrained \
     --state_mask all \
-    --encoder_bottleneck_dim 128 \
+    --encoder_bottleneck_dim 64 \
 #    --debug
