f9bc6ad56bf61aff2e86bd9d6846790dd6b5eb2f
nisutte <nisutte@student.ethz.ch>
2025-08-07 15:09:51 +0200

    new run

--- examples/box_handover_drq/run_actor.sh
+++ examples/box_handover_drq/run_actor.sh
@@ -2,13 +2,11 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@" \
     --actor \
-    --exp_name="Handover V7" \
+    --exp_name="Handover V8" \
     --camera_mode pointcloud \
     --max_traj_length 100 \
     --seed 42 \
     --max_steps 20000 \
-    --random_steps 0 \
-    --training_starts 200 \
     --utd_ratio 8 \
     --batch_size 64 \
     --eval_period 0 \
