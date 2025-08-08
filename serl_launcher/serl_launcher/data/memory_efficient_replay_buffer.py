effd654cc413f877bd480ff911428da9cf02fe66
nisutte <nisutte@student.ethz.ch>
2025-08-08 16:15:34 +0200

    ignore faulty observations

--- serl_launcher/serl_launcher/data/memory_efficient_replay_buffer.py
+++ serl_launcher/serl_launcher/data/memory_efficient_replay_buffer.py
@@ -50,7 +50,14 @@ class MemoryEfficientReplayBuffer(ReplayBuffer):
             next_observation_space=next_observation_space,
         )
 
+    def _obs_is_invalid(self, obs):
+        return np.max(np.abs(obs["state"])) > 1e2
+
     def insert(self, data_dict: DatasetDict):
+        if self._obs_is_invalid(data_dict["observations"]) or self._obs_is_invalid(data_dict["next_observations"]):
+            print("Observation is invalid, skipping insertion.")
+            return
+
         if self._insert_index == 0 and self._capacity == len(self) and not self._first:
             indxs = np.arange(len(self) - self._num_stack, len(self))
             for indx in indxs:
