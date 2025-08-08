0167acbb1300c4302024071020951bdd54a6d060
nisutte <nisutte@student.ethz.ch>
2025-08-08 14:21:33 +0200

    new run & eval sh script

--- examples/box_handover_drq/run_evaluation.sh
+++ examples/box_handover_drq/run_evaluation.sh
@@ -1,17 +1,17 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.5 && \
 python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@" \
     --actor \
     --exp_name="Handover V8" \
     --camera_mode pointcloud \
-    --max_traj_length 100 \
-    --seed 42 \
-    --max_steps 20000 \
-    --utd_ratio 8 \
     --batch_size 64 \
-    --eval_period 0 \
+    --max_traj_length 100 \
+    --checkpoint_path "/home/nico/real-world-rl/serl/examples/box_handover_drq/checkpoints Handover V7 adapt voxel 0731-16:10"\
+    --eval_checkpoint_step 23000 \
+    --eval_n_trajs 20 \
     \
     --encoder_type voxnet-pretrained \
     --state_mask all \
     --encoder_bottleneck_dim 64 \
+    --enable_temporal_ensemble_sampling True \
     --debug
