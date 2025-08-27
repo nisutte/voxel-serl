a642080ffdff81dc571169f1d4751a3923fef3d0
nisutte <nisutte@student.ethz.ch>
2025-08-27 16:06:10 +0200

    eval of v10 run

--- examples/box_handover_drq/run_evaluation.sh
+++ examples/box_handover_drq/run_evaluation.sh
@@ -1,17 +1,17 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.5 && \
-python /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@" \
+uv run /home/nico/real-world-rl/serl/examples/box_handover_drq/drq_policy.py "$@" \
     --actor \
-    --exp_name="Handover V8" \
+    --exp_name="Handover V10" \
     --camera_mode pointcloud \
     --batch_size 64 \
     --max_traj_length 100 \
-    --checkpoint_path "/home/nico/real-world-rl/serl/examples/box_handover_drq/checkpoints Handover V7 adapt voxel 0731-16:10"\
-    --eval_checkpoint_step 23000 \
+    --checkpoint_path "/home/nico/real-world-rl/serl/examples/box_handover_drq/checkpoints Handover V10 random reset 0821-16:58"\
+    --eval_checkpoint_step 11000 \
     --eval_n_trajs 20 \
     \
     --encoder_type voxnet-pretrained \
     --state_mask all \
-    --encoder_bottleneck_dim 64 \
+    --encoder_bottleneck_dim 128 \
     --enable_temporal_ensemble_sampling True \
     --debug
